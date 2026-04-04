a232b2d6f3c56fbf1cb9f74bb7fdcc15013475e4
lenam0811 <110925352+lenam0811@users.noreply.github.com>
2026-04-04 21:40:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
-# Cấu hình các element trên web TikTok
+
 SELECTORS = {
     "login_button": '[data-e2e="top-login-button"]',
     "profile_icon": '[data-e2e="profile-icon"]',
-    
-    # Kẹp nhiều thẻ tìm kiếm lại với nhau: Nếu web đổi tên nút, bot vẫn tìm ra
+
     "save_button": '[data-e2e="browser-book-mark"], [data-e2e="feed-video-save"], [data-e2e="video-save-button"]',
     
     "error_container": '[data-e2e="error-page"]'
 }
 
-# Cài đặt cho Bot
 HEADLESS_MODE = False  
 MIN_DELAY = 3.0        
 MAX_DELAY = 7.0
